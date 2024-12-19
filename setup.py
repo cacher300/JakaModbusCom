f37fd947abf31679967b064cf15790a7824a25d8
cacher300 <77995433+cacher300@users.noreply.github.com>
2024-12-19 08:49:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='JakaModbusCommunication',                     # Your package name
-    version='4.0.4',
+    version='4.0.8',
     author='Lucas Pijl',
     author_email='lapijl@uwaterloo.ca',
     description="A Modbus helper library for Jaka communication.",
     long_description=open("README.md").read(),  # Include README content
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/jaka_modbus_coms",  # Replace with your repo URL
+    url="https://github.com/cacher300/JakaModbusCom",  # Replace with your repo URL
     packages=find_packages(),  # Automatically include all packages
     install_requires=[
-        "pymodbus>=2.5.0",  # Required dependency
+        "pymodbus>=3.8.0",  # Required dependency
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",  # Specify minimum Python version
+    python_requires=">=3.12",  # Specify minimum Python version
 )
 
